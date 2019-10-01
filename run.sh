b21ad12e2480077bcd5d0ba084146427b96c8e69
Jongho Park <jhzpon9@gmail.com>
2019-10-01 17:04:01 +0900

    run scripts

--- run.sh
+++ run.sh
@@ -2,18 +2,13 @@
 
 #export CUDA_VISIBLE_DEVICES="0"
 
-#BATCH_SIZE=4
-#WORKER_SIZE=2
-#MAX_EPOCHS=30
-#LAYER_SIZE=4
-
-#for HIDDEN_SIZE in 384 640 512
-#do
-    #python ./main.py --log_dir log/ls${LAYER_SIZE}_hs${HIDDEN_SIZE} --layer_size ${LAYER_SIZE} --hidden_size ${HIDDEN_SIZE} \
-        #--batch_size $BATCH_SIZE --workers ${WORKER_SIZE} --use_attention --max_epochs $MAX_EPOCHS
-#done
-
-HIDDEN_SIZE=384
+BATCH_SIZE=4
+WORKER_SIZE=2
+MAX_EPOCHS=30
 LAYER_SIZE=4
-nsml run -g 1 -c 4 -d sr-hack-2019-dataset -a "--batch_size ${BATCH_SIZE} --workers ${WORKER_SIZE} --layer_size ${LAYER_SIZE} \
-    --use_attention --max_epochs 20"
+
+for HIDDEN_SIZE in 384 512
+do
+    python ./main.py --log_dir log/ls${LAYER_SIZE}_hs${HIDDEN_SIZE}_1 --layer_size ${LAYER_SIZE} --hidden_size ${HIDDEN_SIZE} \
+        --batch_size $BATCH_SIZE --workers ${WORKER_SIZE} --use_attention --max_epochs $MAX_EPOCHS
+done
--- run_nsml.sh
+++ run_nsml.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-BATCH_SIZE=8
-WORKER_SIZE=2
 GPU_SIZE=1
 CPU_SIZE=4
 DATASET="sr-hack-2019-dataset"
 MAX_EPOCHS=30
 
-nsml run -g $GPU_SIZE -c $CPU_SIZE -d $DATASET -a "--batch_size $BATCH_SIZE --workers $WORKER_SIZE --use_attention --max_epochs $MAX_EPOCHS"
+nsml run -g $GPU_SIZE -c $CPU_SIZE -d $DATASET -a "--batch_size 4 --workers 2 --layer_size 4 --hidden_size 384 \
+    --use_attention --max_epochs 30"
