07668085ffd4c1b962aac4d3acc21514ab160503
Jongho Park <jhzpon9@gmail.com>
2019-10-01 09:54:24 +0900

    run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES="0"
+#export CUDA_VISIBLE_DEVICES="0"
 
 BATCH_SIZE=4
 WORKER_SIZE=2
-MAX_EPOCHS=10
+MAX_EPOCHS=30
+LAYER_SIZE=4
 
-for LAYER_SIZE in 3 4 5 6
+for HIDDEN_SIZE in 384 640 512
 do
-    python ./main.py --log_dir log/ls${LAYER_SIZE} --layer_size ${LAYER_SIZE} --batch_size $BATCH_SIZE --workers $WORKER_SIZE \
+    python ./main.py --log_dir log/ls${LAYER_SIZE}_hs${HIDDEN_SIZE} --layer_size ${LAYER_SIZE} --batch_size $BATCH_SIZE --workers ${WORKER_SIZE} \
         --use_attention --max_epochs $MAX_EPOCHS
 done
+
+#HIDDEN_SIZE=384
+#LAYER_SIZE=4
+#nsml run -g 1 -c 4 -d sr-hack-2019-dataset -a "--batch_size ${BATCH_SIZE} --workers ${WORKER_SIZE} --layer_size ${LAYER_SIZE} \
+    #--use_attention --max_epochs 20"
