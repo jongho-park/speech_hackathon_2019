6564c56ac40ea054f665b7cf1f075c9c22519695
Jongho Park <jhzpon9@gmail.com>
2019-10-02 12:33:39 +0900

    run scripts

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ WORKER_SIZE=2
 MAX_EPOCHS=30
 LAYER_SIZE=4
 
-for HIDDEN_SIZE in 384 512
+for HIDDEN_SIZE in 512
 do
-    python ./main.py --log_dir log/ls${LAYER_SIZE}_hs${HIDDEN_SIZE}_1 --layer_size ${LAYER_SIZE} --hidden_size ${HIDDEN_SIZE} \
-        --batch_size $BATCH_SIZE --workers ${WORKER_SIZE} --use_attention --max_epochs $MAX_EPOCHS
+    python ./main.py --log_dir log/ls${LAYER_SIZE}_hs${HIDDEN_SIZE}_wd --layer_size ${LAYER_SIZE} --hidden_size ${HIDDEN_SIZE} \
+        --batch_size $BATCH_SIZE --workers ${WORKER_SIZE} --use_attention --max_epochs $MAX_EPOCHS --weight_decay 0.0001
 done
--- run_nsml.sh
+++ run_nsml.sh
@@ -3,7 +3,9 @@
 GPU_SIZE=1
 CPU_SIZE=4
 DATASET="sr-hack-2019-dataset"
-MAX_EPOCHS=30
 
-nsml run -g $GPU_SIZE -c $CPU_SIZE -d $DATASET -a "--batch_size 4 --workers 2 --layer_size 4 --hidden_size 384 \
-    --use_attention --max_epochs 30"
+nsml run -g $GPU_SIZE -c $CPU_SIZE -d $DATASET -a "--batch_size 4 --workers 2 --layer_size 4 --use_attention \
+    --hidden_size 512 --max_epochs 100 --load_ckpt team193/sr-hack-2019-dataset/2 model_e29"
+
+#nsml run -g $GPU_SIZE -c $CPU_SIZE -d $DATASET -a "--batch_size 4 --workers 2 --layer_size 4 --use_attention \
+    #--hidden_size 512 --max_epochs 100 --weight_decay 0.0001"
